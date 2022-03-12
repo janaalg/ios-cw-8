79eb1d2513e30cd089297463844d9dfc71ab1104
janaalg <janatariq143@gmail.com>
2022-03-12 12:24:16 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -1,5 +1,22 @@
 import UIKit
+import Foundation
 
 // التمرين الأول
 // ClassWork 1
+
+let name = "jana"
+let birthdayYear = 2005
+var age = 16
+var number = 2022 - birthdayYear
+print("hello, my name is \(name), and my name is \(age)")
+var stringNumber = "8"
+var int = "4"
+var doubleNumber = 2.5
+
+
+
+
+
+
+
 
